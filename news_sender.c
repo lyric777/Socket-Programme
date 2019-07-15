67465963fadc1fe480e6d591fd6ec94fce39aeb9
lyric777 <wysxxuzhili48@163.com>
2019-07-15 05:35:21 -0700

    多播sender和receiver,修正sender

--- news_sender.c
+++ news_sender.c
@@ -26,10 +26,10 @@ int main(int argc, char *argv[])
     send_sock = socket(PF_INET, SOCK_DGRAM, 0);
     memset(&multi_addr, 0, sizeof(multi_addr));
     multi_addr.sin_family = AF_INET;
-    multi_addr.sin_addr.s_addr = inet(argv[1]);  //多播IP
+    multi_addr.sin_addr.s_addr = inet_addr(argv[1]);  //多播IP
     multi_addr.sin_port = htons(atoi(argv[2]));  //多播端口
  
-    setsockopt(send_sock,IPPROTO_IP,IP_MULTICAST_TTL,(void*) &time_live,sizeof(time_alive));
+    setsockopt(send_sock,IPPROTO_IP,IP_MULTICAST_TTL,(void*) &time_live,sizeof(time_live));
     if((fp=fopen("news.txt","r"))==NULL)
 	error_handling("fopen() error");
 
